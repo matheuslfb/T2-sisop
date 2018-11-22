9caca2b88b01d6548631036daef2ec8dc859ac12
Matheus Lima Ferreira <matheus.ferreira.001@acad.pucrs.br>
2018-11-22 16:11:04 -0200

    ajustes

--- src/App.java
+++ src/App.java
@@ -24,33 +24,45 @@ public class App {
 		String enter = s.nextLine(); // le o nome do arquivo de entrada
 		System.out.println();
 		System.out.println("====================================================================================");
-		BufferedReader in = new BufferedReader(new FileReader(enter + ".txt"));
-		String line;
-
-		MODO = Integer.parseInt(in.readLine());
-		start = Integer.parseInt(in.readLine());
-		end = Integer.parseInt(in.readLine());
-
-		Bloco blocoInicial = new Bloco(start, end); // cria o bloco inicial com a memoria disponivel
-
-		Gerente g = new Gerente(blocoInicial); // cria um objeto do tipo do gerente
-
-		int count = 1; // quantidade de solicitacoes de memoria
-
-		int solicitacao = 0;// tamanho da solicitacao de alocacao de memoria para cada bloco
 
 		try {
-			
-			while ((line = in.readLine()) != null) {// faz a leitura do arquivo
-				String info[] = line.split(" ");
-				if (info[0].equals("S")) { // verifica se o comando lido é de solicitacao
 
-					solicitacao = Integer.parseInt(info[1]);// tamanho solicitado do bloco
+			BufferedReader in = new BufferedReader(new FileReader(enter + ".txt"));
+
+			String line;
+
+			MODO = Integer.parseInt(in.readLine());
+			start = Integer.parseInt(in.readLine());
+			end = Integer.parseInt(in.readLine());
+
+			Bloco blocoInicial = new Bloco(start, end); // cria o bloco inicial
+														// com
+														// a memoria disponivel
+
+			Gerente g = new Gerente(blocoInicial); // cria um objeto do tipo do
+													// gerente
+
+			int count = 1; // quantidade de solicitacoes de memoria
+
+			int solicitacao = 0;// tamanho da solicitacao de alocacao de memoria
+								// para cada bloco
+
+			while ((line = in.readLine()) != null) {// faz a leitura do
+													// arquivo
+				String info[] = line.split(" ");
+				if (info[0].equals("S")) { // verifica se o comando lido é
+											// de
+											// solicitacao
+
+					solicitacao = Integer.parseInt(info[1]);// tamanho
+															// solicitado do
+															// bloco
 
 					if (g.verificaMemDisponivel(solicitacao, count)) {
 						g.addBlocoMemoriaOcupada(solicitacao, count);
 						count++;
-						// System.out.println("Tamanho total de memoria disponivel: " +
+						// System.out.println("Tamanho total de memoria
+						// disponivel: " +
 						// g.getMemoriaDisponivel());
 					} else if (!g.verificaMemDisponivel(solicitacao, count)) {
 					}
@@ -59,28 +71,25 @@ public class App {
 					g.liberaBloco(Integer.parseInt(info[1]));
 				}
 			}
-			
-			
+
 			in.close();
-		} catch (FileNotFoundException e) {
-			System.err.print(e);
-		}
-		
-		try {
-			if(!g.atendeListaAguardando(count)) {
-				System.out.println("Não há memoria disponivel! | Total de memoria fragmentada disponivel :"
-						+ g.getMemoriaDisponivel() + " Solicitado: " + solicitacao);
+
+			try {
+				if (!g.atendeListaAguardando(count)) {
+					System.out.println("Não há memoria disponivel! | Total de memoria fragmentada disponivel :"
+							+ g.getMemoriaDisponivel() + " Solicitado: " + solicitacao);
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
 			}
-		} catch (Exception e) {
-			// TODO: handle exception
+
+			g.printListaMemoriaOcupada();
+			g.printListaLivre();
+			System.out.println("Quantidade de solicitações: " + count);
+
+		} catch (FileNotFoundException e) {
+			System.out.println("Erro na leitura do arquivo");
 		}
-		
-			
-		
-		g.printListaMemoriaOcupada();
-		g.printListaLivre();
-		System.out.println("Quantidade de solicitações: " + count);
 
 	}
-
 }
